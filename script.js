f2225ec438bf3a2b8ed334c650275940fe6a44e7
Md Sohel Habib <90446950+Sohelhabib@users.noreply.github.com>
2024-05-25 10:42:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let password = document.getElementById("password");
-let power = document.getElementById("power-point");
+document.addEventListener("DOMContentLoaded", function () {
+    let password = document.getElementById("password");
+    let power = document.getElementById("power-point");
+    let togglePassword = document.getElementById("toggle-password");
 
-password.oninput = function () {
-    let point = 0;
-    let value = password.value;
-    let widthPower = ["1%", "25%", "50%", "75%", "100%"];
-    let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
+    password.oninput = function () {
+        let point = 0;
+        let value = password.value;
+        let widthPower = ["1%", "25%", "50%", "75%", "100%"];
+        let colorPower = ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
 
-    if (value.length >= 6) {
-        let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
-        arrayTest.forEach((item) => {
-            if (item.test(value)) {
-                point += 1;
-            }
-        });
-    }
-    power.style.width = widthPower[point];
-    power.style.backgroundColor = colorPower[point];
-};
+        if (value.length >= 6) {
+            let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+            arrayTest.forEach((item) => {
+                if (item.test(value)) {
+                    point += 1;
+                }
+            });
+        }
+        power.style.width = widthPower[point];
+        power.style.backgroundColor = colorPower[point];
+    };
+
+    togglePassword.addEventListener("click", function () {
+        let type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        this.textContent = type === "password" ? "\u2713" : "\u2715";  // Toggle between tick and cross mark
+    });
+});
